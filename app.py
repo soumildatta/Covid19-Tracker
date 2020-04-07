a39732dbad5a326e92aa0944494cdb92ca1ff250
Soumil Datta <soumildatta@gmail.com>
2020-04-07 17:48:54 -0500

    Home page styling with placeholder data

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 # author: Soumil Datta
 
-from flask import Flask, render_template as render, url_for
+from flask import Flask, render_template as render, url_for, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render("index.html")
+
+@app.route('/statecases', methods=['POST'])
+def statecases():
+    value = request.form
+    return value
 
 if __name__ == "__main__":
     app.run(debug=True)
