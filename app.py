2429334378da80d1f7970e22084bf4b713a2af96
Soumil Datta <soumildatta@gmail.com>
2020-04-07 12:58:53 -0500

    Basic rendering of templates

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 # author: Soumil Datta
 
+from flask import Flask, render_template as render
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
